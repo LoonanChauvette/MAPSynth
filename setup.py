554e1122c73d6842f33a7d3ac219215e2d083e3d
Loonan Chauvette <loonan.chauvette@gmail.com>
2023-02-19 20:45:22 -0500

    ''

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='mapsynth',
+    author='Loonan Chauvette',
+    version='0.0.1',
+    description='MAPLab sound synthesizer',
+    py_modules=["helloworld"],
+    package_dir={'':'mapsynth'}
+)
