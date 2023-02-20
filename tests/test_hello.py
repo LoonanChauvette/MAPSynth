92a916c9dc150da3a1a9b4b1c4d5d9b6eddae8f1
Loonan Chauvette <loonan.chauvette@gmail.com>
2023-02-19 23:57:55 -0500

    'testing'

---
+++ tests/test_hello.py
@@ -0,0 +1,8 @@
+import pytest
+import mapsynth as ms
+
+def test_hello_no_params():
+    assert ms.say_hello() == "Hello, World!"
+
+def test_hello_with_params():
+    assert ms.say_hello("Bob") == "Hello, Bob!"
