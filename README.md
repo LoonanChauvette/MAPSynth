46a726258ac0c0d9f5390d04fb9cee50a2c4672e
Loonan Chauvette <loonan.chauvette@gmail.com>
2023-02-19 23:09:35 -0500

    seting up

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # MAPSynth
-MAPSynth is a Python package for sound synthesis and digital signal processing developed by the Multisensory and Auditory Processing Laboratory (MAPLab).
+MAPSynth is a Python package for sound synthesis and digital signal processing developed 
+by the Multisensory and Auditory Processing Laboratory (MAPLab).
+
+## Installation
+Run the following to install:
+```python
+pip install mapsynth
+```
+
+## Usage
+```python
+import mapsynth as ms
+
+# Do the bare minimum
+ms.say_hello()
+```
+
+## Development
+```bash
+$ pip install -e .[dev]
+
+```
--- setup.py
+++ setup.py
@@ -1,10 +1,32 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='mapsynth',
-    author='Loonan Chauvette',
     version='0.0.1',
     description='MAPLab sound synthesizer',
-    py_modules=["helloworld"],
-    package_dir={'':'mapsynth'}
+    author='Loonan Chauvette',
+    author_email='loonan.chauvette@gmail.com',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT License',
+    py_modules=['hello'],
+    package_dir={'': 'mapsynth'},
+    install_requires=[
+        'numpy',
+        'matplotlib',
+    ],
+    extras_require={
+        "dev": ["pytest>=7"]
+    },
+    classifiers=["License :: OSI Approved :: MIT License",
+                 "Operating System :: OS Independent",
+                 "Programming Language :: Python :: 3",
+                 "Programming Language :: Python :: 3.10",
+                 "Topic :: Multimedia :: Sound/Audio :: Sound Synthesis",
+                 "Development Status :: 1 - Planning",
+                 "Intended Audience :: Science/Research",
+                 "Natural Language :: English"]
 )
